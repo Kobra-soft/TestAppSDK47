e66df960c63236e9283ada62404a0bb40eaa0998
user1 <code.softwaredevg@gmail.com>
2023-12-06 01:53:47 +0000

    Commit - TailwindCSS working

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+/* import { StyleSheet, Text, View } from "react-native"; */
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 items-center justify-center">
+      <View className="bg-gray-100 p-2 rounded-md items-center">
+        <Text className="text-2xl">SDK 47 Test App</Text>
+        <Text className="text-xl">Working with TailwindCSS 3.3.2</Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+/* const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-});
+}); */
